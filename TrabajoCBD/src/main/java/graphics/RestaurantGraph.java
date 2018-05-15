bb23c62e12685cf86e7bb94a106f2914189182fb
samgonzul <34094371+samgonzul@users.noreply.github.com>
2018-05-15 19:48:20 +0200

    Query y gráficas
    Añadidas nuevas queries y nuevas gráficas.

--- TrabajoCBD/src/main/java/graphics/RestaurantGraph.java
+++ TrabajoCBD/src/main/java/graphics/RestaurantGraph.java
@@ -1,11 +1,8 @@
 package graphics;
 
 import java.net.UnknownHostException;
-import java.util.Collection;
 
 import org.jfree.data.category.DefaultCategoryDataset;
-
-import com.mongodb.DBObject;
 
 import util.Queries_restaurant;
 
@@ -15,37 +12,103 @@ public class RestaurantGraph {
 		GraphUtil gu = new GraphUtil();
 		DefaultCategoryDataset data = new DefaultCategoryDataset();
 		Queries_restaurant cr = new Queries_restaurant();
-		Collection<DBObject> restaurantForm = cr.getAllRating();
-		Object[] aux;
-		aux= restaurantForm.toArray();
-		Integer cont1 = 0;
-		Integer cont2 = 0;
-		Integer cont3 = 0;
-		Integer cont4 = 0;
-		Integer cont5 = 0;
-		for(int i = 0; i < restaurantForm.size(); i++) {
-			if(aux[i].equals(Integer.valueOf(1))) {
-				cont1++;
-			}else if(aux[i].equals(Integer.valueOf(2))) {
-				cont2++;
-			}else if(aux[i].equals(Integer.valueOf(3))) {
-				cont3++;
-			}else if(aux[i].equals(Integer.valueOf(4))) {
-				cont4++;
-			}else{
-				cont5++;
-			}
-		}
-		data.addValue(cont1, "Rating", "1");
-		data.addValue(cont2, "Rating", "2");
-		data.addValue(cont3, "Rating", "3");
-		data.addValue(cont4, "Rating", "4");
-		data.addValue(cont5, "Rating", "5");
+		
+		Integer cont1 = cr.findByRating(1.0).count();
+		Integer cont2 = cr.findByRating(2.0).count();
+		Integer cont3 = cr.findByRating(3.0).count();
+		Integer cont4 = cr.findByRating(4.0).count();
+		Integer cont5 = cr.findByRating(5.0).count();
+		Integer cont6 = cr.findByRating(6.0).count();
+		
+		data.addValue(cont1, "Nº de restaurantes", "1");
+		data.addValue(cont2, "Nº de restaurantes", "2");
+		data.addValue(cont3, "Nº de restaurantes", "3");
+		data.addValue(cont4, "Nº de restaurantes", "4");
+		data.addValue(cont5, "Nº de restaurantes", "5");
+		data.addValue(cont6, "Nº de restaurantes", "6");
 		
 		gu.createLineGraph(windowName, graphName, xAxisName, yAxisName, data);
 	}
+	
+	public static void generateRestaurantRatioBarGraph(String windowName, String graphName, String xAxisName, String yAxisName) throws UnknownHostException{
+		GraphUtil gu = new GraphUtil();
+		DefaultCategoryDataset data = new DefaultCategoryDataset();
+		Queries_restaurant cr = new Queries_restaurant();
+		
+		Integer cont1 = cr.findByRating(1.0).count();
+		Integer cont2 = cr.findByRating(2.0).count();
+		Integer cont3 = cr.findByRating(3.0).count();
+		Integer cont4 = cr.findByRating(4.0).count();
+		Integer cont5 = cr.findByRating(5.0).count();
+		Integer cont6 = cr.findByRating(6.0).count();
+		
+		data.addValue(cont1, "Nº de restaurantes", "1");
+		data.addValue(cont2, "Nº de restaurantes", "2");
+		data.addValue(cont3, "Nº de restaurantes", "3");
+		data.addValue(cont4, "Nº de restaurantes", "4");
+		data.addValue(cont5, "Nº de restaurantes", "5");
+		data.addValue(cont6, "Nº de restaurantes", "6");
+		
+		gu.createBarGraph(windowName, graphName, xAxisName, yAxisName, data);
+	}
+	
+	public static void generateRestaurantTypeFoodLineGraph(String windowName, String graphName, String xAxisName, String yAxisName) throws UnknownHostException{
+		GraphUtil gu = new GraphUtil();
+		DefaultCategoryDataset data = new DefaultCategoryDataset();
+		Queries_restaurant cr = new Queries_restaurant();
+		
+		Integer cont1 = cr.findByTypeFood("Thai").count();
+		Integer cont2 = cr.findByTypeFood("Pizza").count();
+		Integer cont3 = cr.findByTypeFood("Curry").count();
+		Integer cont4 = cr.findByTypeFood("African").count();
+		Integer cont5 = cr.findByTypeFood("Chinese").count();
+		Integer cont6 = cr.findByTypeFood("Turkish").count();
+		Integer cont7 = cr.findByTypeFood("Breakfast").count();
+		Integer cont8 = cr.findByTypeFood("American").count();
+		
+		data.addValue(cont1, "Nº de restaurantes", "Thai");
+		data.addValue(cont2, "Nº de restaurantes", "Pizza");
+		data.addValue(cont3, "Nº de restaurantes", "Curry");
+		data.addValue(cont4, "Nº de restaurantes", "African");
+		data.addValue(cont5, "Nº de restaurantes", "Chinese");
+		data.addValue(cont6, "Nº de restaurantes", "Turkish");
+		data.addValue(cont7, "Nº de restaurantes", "Breakfast");
+		data.addValue(cont8, "Nº de restaurantes", "American");
+		
+		gu.createLineGraph(windowName, graphName, xAxisName, yAxisName, data);
+	}
+	
+	public static void generateRestaurantTypeFoodBarGraph(String windowName, String graphName, String xAxisName, String yAxisName) throws UnknownHostException{
+		GraphUtil gu = new GraphUtil();
+		DefaultCategoryDataset data = new DefaultCategoryDataset();
+		Queries_restaurant cr = new Queries_restaurant();
+		
+		Integer cont1 = cr.findByTypeFood("Thai").count();
+		Integer cont2 = cr.findByTypeFood("Pizza").count();
+		Integer cont3 = cr.findByTypeFood("Curry").count();
+		Integer cont4 = cr.findByTypeFood("African").count();
+		Integer cont5 = cr.findByTypeFood("Chinese").count();
+		Integer cont6 = cr.findByTypeFood("Turkish").count();
+		Integer cont7 = cr.findByTypeFood("Breakfast").count();
+		Integer cont8 = cr.findByTypeFood("American").count();
+		
+		data.addValue(cont1, "Nº de restaurantes", "Thai");
+		data.addValue(cont2, "Nº de restaurantes", "Pizza");
+		data.addValue(cont3, "Nº de restaurantes", "Curry");
+		data.addValue(cont4, "Nº de restaurantes", "African");
+		data.addValue(cont5, "Nº de restaurantes", "Chinese");
+		data.addValue(cont6, "Nº de restaurantes", "Turkish");
+		data.addValue(cont7, "Nº de restaurantes", "Breakfast");
+		data.addValue(cont8, "Nº de restaurantes", "American");
+		
+		gu.createBarGraph(windowName, graphName, xAxisName, yAxisName, data);
+	}
+	
 	public static void main(String[] args) throws UnknownHostException {
-		RestaurantGraph.generateRestaurantRatioLineGraph("Gráfica", "Rating de restaurantes", "Rating", "Nº de restaurantes");
+		//RestaurantGraph.generateRestaurantRatioLineGraph("Gráfica", "Rating de restaurantes", "Rating", "Nº de restaurantes");
+		//RestaurantGraph.generateRestaurantRatioBarGraph("Gráfica", "Rating de restaurantes", "Rating", "Nº de restaurantes");
+		//RestaurantGraph.generateRestaurantTypeFoodLineGraph("Gráfica", "Tipos de comida por restaurantes", "Tipos de comida", "Nº de restaurantes");
+		//RestaurantGraph.generateRestaurantTypeFoodBarGraph("Gráfica", "Tipos de comida por restaurantes", "Tipos de comida", "Nº de restaurantes");
 	}
 
 }
--- TrabajoCBD/src/main/java/util/Queries_restaurant.java
+++ TrabajoCBD/src/main/java/util/Queries_restaurant.java
@@ -22,8 +22,29 @@ public class Queries_restaurant {
 		result = Lists.newArrayList(agout.results());
 		return result;
 	}
+	//Devuelve todos los elementos de la BD
+	public DBCursor findAll() throws UnknownHostException{
+		DBCollection collection = DatabaseService.getCollection("Restaurantes");
+		DBObject query = new BasicDBObject();
+		DBCursor result = collection.find(query);
+		return result;
+	}
+	//Devuelve todos los elementos por rating
+	public DBCursor findByRating(Double rating) throws UnknownHostException {
+		DBCollection collection = DatabaseService.getCollection("Restaurantes");
+		DBObject query = new BasicDBObject("rating", rating);
+		DBCursor res = collection.find(query);
+		return res;
+	}
+	//Devuelve todos los elementos por tipo de comida
+	public DBCursor findByTypeFood(String type) throws UnknownHostException{
+		DBCollection collection = DatabaseService.getCollection("Restaurantes");
+		DBObject query = new BasicDBObject("type_of_food",java.util.regex.Pattern.compile(type));
+		DBCursor result = collection.find(query);
+		return result;
+	}
 	
-	//Encuentra un objeto dado un Codigo Postal. String
+	//Encuentra un objeto dado un Codigo Postal
 	public DBCursor findByLocationCP(String number) throws UnknownHostException{
 		DBCollection collection = DatabaseService.getCollection("Restaurantes");
 		DBObject query = new BasicDBObject("postcode",java.util.regex.Pattern.compile(number));
@@ -31,10 +52,31 @@ public class Queries_restaurant {
 		return result;
 	}
 	
+	//Encuentra un objeto dada una ciudad
+	public DBCursor findByCity(String ciudad) throws UnknownHostException{
+		DBCollection collection = DatabaseService.getCollection("Restaurantes");
+		DBObject query = new BasicDBObject("address line 2",java.util.regex.Pattern.compile(ciudad));
+		DBCursor result = collection.find(query);
+		return result;
+	}
+	
 	public static void main(String[] args) throws UnknownHostException {
 		Queries_restaurant qr = new Queries_restaurant();
 		//Collection<DBObject> res = qr.getAllRating();
-		DBCursor aux = qr.findByLocationCP("8NX");
-		System.out.println(aux);
+//		DBCursor aux = qr.findByLocationCP("8NX");
+//		while(aux.hasNext()) {
+//			System.out.println(aux.next());
+//		}
+//		DBCursor res = qr.findAll();
+//		System.out.println(res.count());
+//		DBCursor aux2 = qr.findByRating(6.0);
+//		System.out.println(aux2.count());
+//		DBCursor res = qr.findByTypeFood("Thai");
+//		System.out.println(res.count());
+		DBCursor res = qr.findByCity("London");
+		while(res.hasNext()) {
+			//System.out.println(res.next().toString());
+			System.out.println(res.next());
+		}
 	}
 }
